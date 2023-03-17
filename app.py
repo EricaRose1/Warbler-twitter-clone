120422ad388c9c7a961f1b0ad15822a4c57b73f7
EricaRose1 <115095732+EricaRose1@users.noreply.github.com>
2023-03-17 11:51:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,353 +1,166 @@
-import os
-
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import Unauthorized
 
-from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
-from models import db, connect_db, User, Message
+from models import db, connect_db, User, Feedback
+from forms import UserForm, LoginForm, FeedbackForm
 
-
-CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
-
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///users_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 
-
-##############################################################################
-# User signup/login/logout
+toolbar = DebugToolbarExtension(app)
 
 
-@app.before_request
-def add_user_to_g():
-    """If we're logged in, add curr user to Flask global."""
+@app.route('/')
+def home_page():
+    '''hompeage'''
+    return redirect('/register')
 
-    if CURR_USER_KEY in session:
-        g.user = User.query.get(session[CURR_USER_KEY])
+#####
 
-    else:
-        g.user = None
-
-
-def do_login(user):
-    """Log in user."""
-
-    session[CURR_USER_KEY] = user.id
-
-
-def do_logout():
-    """Logout user."""
-
-    if CURR_USER_KEY in session:
-        del session[CURR_USER_KEY]
-
-
-@app.route('/signup', methods=["GET", "POST"])
-def signup():
-    """Handle user signup.
-
-    Create new user and add to DB. Redirect to home page.
-
-    If form not valid, present form.
-
-    If the there already is a user with that username: flash message
-    and re-present form.
-    """
-    if CURR_USER_KEY in session:
-        del session[CURR_USER_KEY]
-
-    form = UserAddForm()
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    '''register a user: produce form & handle form submission'''
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+        
+    form = UserForm()
 
     if form.validate_on_submit():
-        try:
-            user = User.signup(
-                username=form.username.data,
-                password=form.password.data,
-                email=form.email.data,
-                image_url=form.image_url.data or User.image_url.default.arg,
-            )
-            db.session.commit()
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
 
-        except IntegrityError:
-            flash("Username already taken", 'danger')
-            return render_template('users/signup.html', form=form)
+        new_user = User.register(username, password, first_name, last_name, email)
+        db.session.commit()
+        session['username'] = new_user.username
 
-        do_login(user)
-
-        return redirect("/")
-
+        return redirect(f"/users/{new_user.username}")
     else:
-        return render_template('users/signup.html', form=form)
+        return render_template('/users/register.html', form=form)
 
+###
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    """Handle user login."""
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
 
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.authenticate(form.username.data,
-                                 form.password.data)
+        username = form.username.data
+        password = form.password.data
+        user = User.authenticate(username, password)
 
         if user:
-            do_login(user)
-            flash(f"Hello, {user.username}!", "success")
-            return redirect("/")
-
-        flash("Invalid credentials.", 'danger')
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ['Invalid username/password.']
+            return render_template("users/login.html", form=form)
 
     return render_template('users/login.html', form=form)
 
-
+###
 @app.route('/logout')
 def logout():
-    """Handle logout of user."""
-    session.pop('user', None)
-    flash(f'Logged out', 'sucess')
+    '''logout'''
+    session.pop("username")
     return redirect('/login')
+
+###
+@app.route('/users/<username>')
+def show_user(username):
+    '''example page for logged-in-users.'''
+    if 'username' not in session or username != session['username']:
+        raise Unauthorized()
     
-    # IMPLEMENT THIS
+    user = User.query.get(username)
 
 
-##############################################################################
-# General user routes:
+    return render_template('users/show.html', user=user)
 
-@app.route('/users')
-def list_users():
-    """Page with listing of users.
-
-    Can take a 'q' param in querystring to search by that username.
-    """
-
-    search = request.args.get('q')
-
-    if not search:
-        users = User.query.all()
-    else:
-        users = User.query.filter(User.username.like(f"%{search}%")).all()
-
-    return render_template('users/index.html', users=users)
-
-
-@app.route('/users/<int:user_id>')
-def users_show(user_id):
-    """Show user profile."""
-
-    user = User.query.get_or_404(user_id)
-
-    # snagging messages in order from the database;
-    # user.messages won't be in order by default
-    messages = (Message
-                .query
-                .filter(Message.user_id == user_id)
-                .order_by(Message.timestamp.desc())
-                .limit(100)
-                .all())
-    return render_template('users/show.html', user=user, messages=messages)
-
-
-@app.route('/users/<int:user_id>/following')
-def show_following(user_id):
-    """Show list of people this user is following."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    user = User.query.get_or_404(user_id)
-    return render_template('users/following.html', user=user)
-
-
-@app.route('/users/<int:user_id>/followers')
-def users_followers(user_id):
-    """Show list of followers of this user."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    user = User.query.get_or_404(user_id)
-    return render_template('users/followers.html', user=user)
-
-
-@app.route('/users/follow/<int:follow_id>', methods=['POST'])
-def add_follow(follow_id):
-    """Add a follow for the currently-logged-in user."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    followed_user = User.query.get_or_404(follow_id)
-    g.user.following.append(followed_user)
-    db.session.commit()
-
-    return redirect(f"/users/{g.user.id}/following")
-
-
-@app.route('/users/stop-following/<int:follow_id>', methods=['POST'])
-def stop_following(follow_id):
-    """Have currently-logged-in-user stop following this user."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    followed_user = User.query.get(follow_id)
-    g.user.following.remove(followed_user)
-    db.session.commit()
-
-    return redirect(f"/users/{g.user.id}/following")
-
-
-@app.route('/users/profile', methods=["GET", "POST"])
-def profile():
-    """Update profile for current user."""
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
+###
+@app.route('/users/<username>/delete', methods=['POST'])
+def remove_user(username):
+    '''remove user and redirect to login'''
+    if 'username' not in session or username != session['username']:
+        raise Unauthorized()
     
-    user = g.user
-    form = UserEditForm(obj = user)
+    user = User.query.get(username)
+    db.session.delete(user)
+    db.session.commit()
+    session.pop("username")
+
+    return redirect("/login")
+
+
+
+
+@app.route('/users/<username>/feedback/add', methods=['GET','POST'])
+def feedback_form(username):
+    ''' display form to add feedback'''
+    if 'username' not in session or username != session['username']:
+        raise Unauthorized()
+
+    form = FeedbackForm()
 
     if form.validate_on_submit():
-        if User.authenticate(user.username, form.password.data)
-            user.username = form.username.data
-            user.email = form.email.data
-            user.image_url = form.image_url.data or '/static/images/defualt-pic.png'
-            user.header_image_url = form.header_image_url.data or '/static/images/warler-hero.jpg'
-            user.bio = form.bio.data
+        title = form.title.data
+        content = form.content.data
+        username = session['username']
 
-            db.session.commit()
-            return redirect(f'/user/{user.id}')
-        
-        flash('Wrong password, please try again', 'danger')
+        feedback = Feedback(title=title, content=content,username=username)
 
-    return render_template('users/edit.html', form=form, user_id=user.id)
-
-
-@app.route('/users/delete', methods=["POST"])
-def delete_user():
-    """Delete user."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    do_logout()
-
-    db.session.delete(g.user)
-    db.session.commit()
-
-    return redirect("/signup")
-
-
-##############################################################################
-# Messages routes:
-
-@app.route('/messages/new', methods=["GET", "POST"])
-def messages_add():
-    """Add a message:
-
-    Show form if GET. If valid, update message and redirect to user page.
-    """
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    form = MessageForm()
-
-    if form.validate_on_submit():
-        msg = Message(text=form.text.data)
-        g.user.messages.append(msg)
+        db.session.add(feedback)
         db.session.commit()
 
-        return redirect(f"/users/{g.user.id}")
-
-    return render_template('messages/new.html', form=form)
-
-
-@app.route('/messages/<int:message_id>', methods=["GET"])
-def messages_show(message_id):
-    """Show a message."""
-
-    msg = Message.query.get(message_id)
-    return render_template('messages/show.html', message=msg)
-
-
-@app.route('/messages/<int:message_id>/delete', methods=["POST"])
-def messages_destroy(message_id):
-    """Delete a message."""
-
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
-    msg = Message.query.get(message_id)
-    db.session.delete(msg)
-    db.session.commit()
-
-    return redirect(f"/users/{g.user.id}")
-
-
-##############################################################################
-# Homepage and error pages
-
-
-@app.route('/')
-def homepage():
-    """Show homepage:
-
-    - anon users: no messages
-    - logged in: 100 most recent messages of followed_users
-    """
-
-    if g.user:
-        following_ids = [f.id for f in g.user.following] + [g.user.id]
-        messages = (Message
-                    .query
-                    .order_by(Message.timestamp.desc())
-                    .limit(100)
-                    .all())
-
-        return render_template('home.html', messages=messages)
-
+        return redirect(f'/users/{feedback.id}')
     else:
-        return render_template('home-anon.html')
+        return render_template('/feedback/add.html', form=form)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    '''404 not found page'''
+
+@app.route('/feedback/<int:feedback_id>/update', methods=['GET', 'POST'])
+def update_feedback(feedback_id):
+    '''show update-feedback form & process it.'''
+
+    feedback = Feedback.query.get(id)
+
+    if "username" not in session or feedback.username != session['username']:
+        raise Unauthorized()
+
+    form = FeedbackForm(obj = feedback)
+
+    if form.validate_on_submit():
+        feedback.title = form.title.data
+        feedback.content = form.content.data
+
+        db.session.commit()
+
+        return redirect(f'/users/{feedback.username}')
+    return render_template("/feedback/edit.html", form=form, feedback=feedback)
+
+@app.route('/feedback/<int:feedback_id>/delete', methods=['POST'])
+def delete_feedback(feedback_id):
+    '''delete feedback'''
+    feedback = Feedback.query.get_or_404(feedback_id)
+    if 'username' not in session or feedback.username != session['username']:
+        raise Unauthorized()
     
-    return render_template('404.html'), 404
+   
+    if form.validate_on_submit():
+        db.session.delete(feedback)
+        db.session.commit()
 
-##############################################################################
-# Turn off all caching in Flask
-#   (useful for dev; in production, this kind of stuff is typically
-#   handled elsewhere)
-#
-# https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+    return redirect(f'/users/{feedback.username}')   
 
-@app.after_request
-def add_header(req):
-    """Add non-caching headers on every request."""
-
-    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    req.headers["Pragma"] = "no-cache"
-    req.headers["Expires"] = "0"
-    req.headers['Cache-Control'] = 'public, max-age=0'
-    return req
